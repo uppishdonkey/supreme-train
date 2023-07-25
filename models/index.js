c190ac8a871462afd2773dea0b69a36a968694d2
jbarz <jbarz0415@gmail.com>
2023-07-24 20:58:00 -0400

    columns defined in product.js

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Product, ProductTag, {
+  foreignKey: 'product_id',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, ProductTag, {
+  foreignKey: 'product_id',
+});
 module.exports = {
   Product,
   Category,
